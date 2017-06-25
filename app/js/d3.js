27bced5f7fe4cbd84331061cda9c07dc4a39ae9b
dagrende <dag.rende@find-out.se>
2017-06-25 19:27:41 +0200

    separated graph-renderer from main, cleaned up code

--- app/js/d3.js
+++ app/js/d3.js
@@ -1,3 +1,4 @@
 export * from 'd3-selection';
+export * from 'd3-selection-multi';
 export * from 'd3-force';
 export * from 'd3-request';
---
+++ app/js/graph-renderer.js
@@ -0,0 +1,85 @@
+import * as d3 from './d3';
+import _ from 'lodash';
+
+export default function(graphSite) {
+  var r = 20,
+  width = 800,
+  height = 300;
+
+  var svg = d3.select(graphSite).append('svg')
+  .attr('width', width).attr('height', height);
+
+  svg.append('defs').append('marker')
+  .attr("id", 'markerArrowEnd')
+  .attr("viewBox", "0 -5 10 10")
+  .attr("refX", 10)
+  .attr("refY", 0)
+  .attr("markerWidth", 8)
+  .attr("markerHeight", 8)
+  .attr("orient", "auto")
+  .append("path")
+  .attr("d", 'M0,-5 L10,0 L0,5')
+  .attr('fill', 'black');
+
+  function render(data) {
+    // run autolayout
+    var simulation = d3.forceSimulation(data.nodes)
+    .force('center', d3.forceCenter(width / 2, height / 2))
+    .force("charge", d3.forceManyBody().distanceMin(r * 2))
+    .force("link", d3.forceLink(data.links).id(d => d.id).distance(70))
+    .force("collide",d3.forceCollide())
+    .on("tick", tick);
+
+    // render nodes
+    var nodes = svg.selectAll('.node').data(data.nodes, function(d) {return d.id});
+    nodes.exit().remove();
+    var nodesEnter = nodes.enter().append('g')
+    .attr('class', 'node')
+    .attr('transform', 'translate(50, 50)');
+    nodesEnter.append('circle')
+    .attr('r', r);
+    nodesEnter.append('text');
+    var enterUpdate = nodesEnter.merge(nodes);
+    enterUpdate.select('text').text(function(d) {return d.label});
+    enterUpdate.select('text').each(function(d) {
+      var bbox = this.getBBox();
+      d3.select(this).attr('x', -bbox.x - bbox.width / 2);
+      d3.select(this).attr('y', -bbox.y - bbox.height / 2);
+    });
+
+    // render links
+    var links = svg.selectAll('.link').data(data.links, function(d) {return String(d.source) + '_' + String(d.target)});
+    links.exit().remove();
+    var linksEnter = links.enter().append('line')
+    .attr('class', 'link')
+    .attr('marker-end', 'url(#markerArrowEnd)');
+    var linksEnterUpdate = linksEnter.merge(links);
+
+    // returns {from: {x, y}, to: {x, y}} that goes between
+    // edge of circles with the supplied center points
+    function adjustEnds(fromCenterPoint, toCenterPoint, r) {
+      var dx = toCenterPoint.x - fromCenterPoint.x,
+      dy = toCenterPoint.y - fromCenterPoint.y,
+      length = Math.sqrt(dx * dx + dy * dy);
+      dx = dx / length * r;
+      dy = dy / length * r;
+      return {from: {x: fromCenterPoint.x + dx, y: fromCenterPoint.y + dy}, to: {x: toCenterPoint.x - dx, y: toCenterPoint.y - dy}};
+    }
+
+    // autolayout animation
+    function tick() {
+      enterUpdate.each(function(d) {
+        d3.select(this).attr('transform', 'translate(' + d.x + ', ' + d.y + ')');
+      });
+      linksEnterUpdate.each(function(d) {
+        var adjustedEnds = adjustEnds(d.source, d.target, r);
+        d3.select(this)
+        .attr("x1", function(d) { return adjustedEnds.from.x; })
+        .attr("y1", function(d) { return adjustedEnds.from.y; })
+        .attr("x2", function(d) { return adjustedEnds.to.x; })
+        .attr("y2", function(d) { return adjustedEnds.to.y; });
+      });
+    }
+  }
+  return {render: render};
+}
